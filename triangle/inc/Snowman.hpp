f71e8a383e93af8ddd37cfc0d27d89e5c789293c
Evan Wilde <etcwilde@uvic.ca>
2015-02-18 16:42:05 -0800

    Adding Snowman model, moved field function implementation

---
+++ triangle/inc/Snowman.hpp
@@ -0,0 +1,16 @@
+#ifndef SNOWMAN_H
+#define SNOWMAN_H
+
+#include "ImplicitObject.hpp"
+#include "ImplicitSphere.hpp"
+#include "fieldFunctions.hpp"
+
+namespace Implicit
+{
+	class Snowman : public Model
+	{
+	public:
+		Snowman();
+	};
+};
+#endif //SNOWMAN_H
---
+++ triangle/src/Snowman.cpp
@@ -0,0 +1,12 @@
+#include "Snowman.hpp"
+using namespace Implicit;
+
+Snowman::Snowman() :
+	Model()
+{
+	Object* body_sphere = new Sphere(geoffFunction, ColorRGB(1, 1, 1), 1.f);
+	Object* button_sphere = new Sphere(wyvillFunction, ColorRGB(1, 0, 0),
+			0.1f);
+	addObject(body_sphere);
+	addObject(button_sphere);
+}
