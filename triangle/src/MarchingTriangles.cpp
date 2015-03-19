6f7e0f9fc3de518211baadd02ff310d62b5e968e
Evan Wilde <etcwilde@uvic.ca>
2015-03-18 17:15:32 -0700

    Gives correct Normals on vertices and faces

--- triangle/src/MarchingTriangles.cpp
+++ triangle/src/MarchingTriangles.cpp
@@ -6,7 +6,7 @@
  * Date:	Mar 16 2015
  */
 
-#define DEPTH 3
+#define DEPTH 1
 
 #include "MarchingTriangles.hpp"
 
@@ -38,16 +38,19 @@ void MarchingTriangles::Polygonize()
 {
 	SeedTriangle();
 
+
 	unsigned int depth = 0;
-	while(depth <= DEPTH && m_front.size() > 0)
+	while(depth < DEPTH && m_front.size() > 0)
 	{
 #ifdef DEBUG
 		std::cout << "Polygonization Depth: " << depth << '\n';
 		std::cout << "Front size: " << m_front.size() << '\n';
 #endif
 		Edge e = m_front.pop_edge();
-		std::cout << "Edge: " << e.pt1 << ", " << e.pt2 << '\n';
 
+#ifdef DEBUG
+		std::cout << "Edge: " << e.pt1 << ", " << e.pt2 << '\n';
+#endif
 		glm::vec3 p1 = e.pt1;
 		glm::vec3 p2 = e.pt2;
 		glm::vec3 n1 = m_object->Normal(p1);
@@ -55,15 +58,15 @@ void MarchingTriangles::Polygonize()
 		glm::vec3 T, B;
 		getTangentSpace(n1, T, B);
 		glm::vec3 p3;
-		if (glm::dot(T, e.direction()) > 0)
-			p3 = m_object->Project(((p1 + p2) / 2.f) + T * 0.2f);
+		if (glm::dot(T, e.direction()) < 0)
+			p3 = m_object->Project(((p1 + p2) / 2.f) + (T * -0.2f));
 		else
-			p3 = m_object->Project(((p1 + p2) / 2.f) + T * -0.2f);
+			p3 = m_object->Project(((p1 + p2) / 2.f) + (T * 0.2f));
 
 		m_mesh.AddFace(p1, p2, p3, n1, n2, m_object->Normal(p3));
 
 		m_front.push_edge(Edge(p1, p3));
-		m_front.push_edge(Edge(p2, p3));
+		//m_front.push_edge(Edge(p3, p2));
 
 #ifdef DEBUG
 	std::cout
@@ -94,7 +97,7 @@ void MarchingTriangles::SeedTriangle()
 
 	glm::vec3 p2 = m_object->Project(start_vertex + (T * 0.2f));
 	glm::vec3 p3 = m_object->Project(((start_vertex + p2) / 2.f) + B * 0.2f);
-	glm::vec3 p4 = m_object->Project(((start_vertex + p2) / 2.f) - B * 0.2f);
+	// glm::vec3 p4 = m_object->Project(((start_vertex + p2) / 2.f) - B * 0.2f);
 
 #ifdef DEBUG
 	std::cout
@@ -107,27 +110,36 @@ void MarchingTriangles::SeedTriangle()
 	e1.pt2 = p3;
 
 	Edge e2;
-	e2.pt1 = p2;
-	e2.pt2 = p3;
+	e2.pt2 = p2;
+	e2.pt1 = p3;
 
-	Edge e3;
-	e3.pt1 = start_vertex;
-	e3.pt2 = p4;
+	/* Edge e3;
+	e3.pt2 = start_vertex;
+	e3.pt1 = p4;
 
 	Edge e4;
 	e4.pt1 = p2;
-	e4.pt2 = p4;
+	e4.pt2 = p4; */
 
 	m_front.push_edge(e1);
 	m_front.push_edge(e2);
-	m_front.push_edge(e3);
-	m_front.push_edge(e4);
 
+	glm::vec3 n1 = m_object->Normal(start_vertex);
 	glm::vec3 n2 = m_object->Normal(p2);
 	glm::vec3 n3 = m_object->Normal(p3);
-	glm::vec3 n4 = m_object->Normal(p4);
-	m_mesh.AddFace(start_vertex, p2, p3, start_normal, n2, n3);
-	m_mesh.AddFace(start_vertex, p2, p4, start_normal, n2, n4);
+
+#ifdef DEBUG
+	std::cout << "Triangle 1: " << start_vertex << ", "
+		<< p2 << ", "
+		<< p3 << ". "
+		<< n1 << ", " << n2 << ", " << n3 << '\n';
+
+/*	std::cout << "Triangle 2: " << start_vertex << ", "
+		<< p4 << ", "
+		<< p2 << ". "
+		<< n1 << ", " << n4 << ", " << n2 << '\n'; */
+#endif
+	m_mesh.AddFace(start_vertex, p2, p3, n1, n2, n3);
 }
 
 
