d42b99faa1e55f497fbf792be4231a91babee7b9
Evan Wilde <etcwilde@uvic.ca>
2015-05-08 12:01:01 -0700

    KD Tree Data container
    I've added a container for all the data members of the polygonizer. This allows
    the construction of a single kd tree for the polygonizer to use.

--- inc/PolyFront.hpp
+++ inc/PolyFront.hpp
@@ -14,13 +14,55 @@ public:
 	inline unsigned int operator[](unsigned int i)
 	{ return m_verts[i].v_index; }
 
+	inline unsigned int verts() const { return m_verts.size(); }
+
+	inline unsigned int getVertex(unsigned int index) const
+	{ return m_verts[index].v_index; }
+
+	inline void setOpenAngle(unsigned int vertex_index, float angle)
+	{ m_verts[vertex_index].open_angle = angle; }
+
+	inline float getOpenAngle(unsigned int vertex_index) const
+	{ return m_verts[vertex_index].open_angle; }
+
+	inline void setRoc(unsigned int vertex_index, float roc)
+	{ m_verts[vertex_index].roc = roc; }
+
+	inline float getRoc(unsigned int vertex_index) const
+	{ return m_verts[vertex_index].roc; }
+
+	// TODO: Put implementation in cpp file
+	// Index Vertex: Index in TrisPolyVerts
+	// neighbors: Index to verts in m_verts list
+	void pushVertex(unsigned int vertex_index,
+			unsigned int *neighbors = NULL,
+			float open_angle = 0,
+			float roc=1)
+	{
+		FrontVertex v;
+		v.v_index = vertex_index;
+		if (neighbors)
+		{
+			v.neighbors[0] = neighbors[0];
+			v.neighbors[1] = neighbors[1];
+		}
+		v.open_angle = open_angle;
+		v.roc = roc;
+		m_verts.push_back(v);
+	}
+
+
 protected:
 
 private:
 	typedef struct FrontVertex
 	{
 		unsigned int v_index;
-		struct FrontVertex* neighbors[2];
+		unsigned int neighbors[2];
+		// neighbors[0] is the left
+		// neighbors[1] is the right
+		float open_angle;
+		float roc; // Radius of curvature
 	} FrontVertex;
 
 private:
--- inc/PolyTris.hpp
+++ inc/PolyTris.hpp
@@ -5,12 +5,12 @@
 #include "Polygonizer.hpp"
 #include "PolyTris.hpp"
 #include "PolyFront.hpp"
+#include "PolyEdge.hpp"
 #include "Mesh.hpp"
 #include "utils/GeoCore.hpp"
 #include "utils/MathCore.hpp"
 #include "nanoflann.hpp"
 
-#include "PolyEdge.hpp"
 
 #include <list>
 #include <vector>
@@ -18,25 +18,127 @@
 
 #include <glm/glm.hpp>
 
-// Curvature Dependent Marching Triangles Polygonizer
-class TrisPoly : public Polygonizer
+class PolyContainer
 {
 public:
-	// Mush Have
-	TrisPoly(Implicit::Object& o);
+	PolyContainer() { }
 
 	unsigned int verts() const { return m_vertices.size(); }
 	unsigned int edges() const { return m_edges.size(); }
 	unsigned int fronts() const { return m_fronts.size(); }
 
-	const glm::vec3& getVertex(unsigned int index) const;
-	const Edge& getEdge(unsigned int index) const;
-	const Front* getFront(unsigned int index) const;
+	const glm::vec3& getVertex(unsigned int index) const
+	{ return m_vertices[index]; }
+
+	const Edge& getEdge(unsigned int index) const
+	{ return m_edges[index]; }
+
+	// Gets the top front
+	const Front* getFront() const { return m_fronts.back(); }
+
+	const Front* getFront(unsigned int index) const
+	{ return m_fronts[index];}
+
+	Front* getFront() { return m_fronts.back(); }
 
 
+	void popFront() { m_fronts.pop_back(); }
+
+// I may make these public
+private:
+	std::vector<glm::vec3> m_vertices;
+	std::vector<Edge> m_edges;
+	std::vector<Front*> m_fronts;
+};
+
+
+// For translating the mesh points to a kdtree
+class MeshPtAdaptor
+{
+public:
+	MeshPtAdaptor(const PolyContainer& poly): m_pts(poly) { }
+
+	inline unsigned int kdtree_get_point_count() const
+	{ return m_pts.verts(); }
+
+	inline float kdtree_distance(const float* p1, const unsigned int index,
+			unsigned int) const
+	{
+		return glm::length(glm::vec3(p1[0], p1[1], p1[2]) - m_pts.getVertex(index));
+	}
+
+	inline float kdtree_get_pt(const unsigned int i, int dim) const
+	{
+		if(dim == 0) return m_pts.getVertex(i).x;
+		else if(dim == 1) return m_pts.getVertex(i).y;
+		else return m_pts.getVertex(i).z;
+	}
+
+	template <class BBOX>
+	bool kdtree_get_bbox(BBOX&) const { return false; }
+
+private:
+	const PolyContainer& m_pts;
+
+};
+
+// Translating the front points to a kd tree
+class FrontPtAdaptor
+{
+public:
+	FrontPtAdaptor(const PolyContainer& poly) : m_pts(poly) { }
+
+	inline unsigned int kdtree_get_point_count() const
+	{ return m_pts.getFront()->verts(); }
+
+	inline float kdtree_distance(const float* p1, const unsigned int index, unsigned int) const
+	{
+		return glm::length(
+				glm::vec3(p1[0], p1[1], p1[2]) -
+				m_pts.getVertex(m_pts.getFront()->getVertex(index)));
+	}
+
+
+	inline float kdtree_get_pt(const unsigned int i, int dim) const
+	{
+		if (dim == 0) return m_pts.getVertex(m_pts.getFront()->getVertex(i)).x;
+		else if (dim == 1) return m_pts.getVertex(m_pts.getFront()->getVertex(i)).y;
+		else return m_pts.getVertex(m_pts.getFront()->getVertex(i)).z;
+	}
+
+	template <class BBOX>
+	bool kdtree_get_bbox(BBOX&) const {return false;}
+
+private:
+	const PolyContainer& m_pts;
+};
+
+// Mesh Pt tree
+typedef nanoflann::KDTreeSingleIndexAdaptor<
+	nanoflann::L2_Simple_Adaptor<float, MeshPtAdaptor>,
+	MeshPtAdaptor, 3> Mesh_pt_kdtree;
+
+typedef nanoflann::KDTreeSingleIndexAdaptor<
+	nanoflann::L2_Simple_Adaptor<float, FrontPtAdaptor>,
+	FrontPtAdaptor, 3> Front_pt_kdtree;
+
+// Curvature Dependent Marching Triangles Polygonizer
+class TrisPoly : public Polygonizer
+{
+public:
+	TrisPoly(Implicit::Object& o):
+		Polygonizer(o),
+		m_mesh_adaptor(m_container),
+		m_front_adaptor(m_container),
+		m_mesh_tree(2, m_mesh_adaptor ,
+				nanoflann::KDTreeSingleIndexAdaptorParams(10)),
+		m_front_tree(2, m_front_adaptor,
+				nanoflann::KDTreeSingleIndexAdaptorParams(10))
+
+	{ }
 
 protected:
-	// Must have
+	// Must have for polygonizer
 	virtual Mesh polygonize();
 private:
 	//Methods
@@ -51,15 +153,28 @@ private:
 	// Only works on fronts with 3 or 4 vertices (a hole)
 	void fill(Front* f);
 
+	// Generate and set open angles in the front
+	void actualizeAngles();
+
+	// Gets the radius of curvature at a point
+	// Maximum absolute curvature used to calculate
+	float rocAtPt(const glm::vec3& v);
+
 private:
 	// Variables
 	Mesh m_mesh;
 
-	std::vector<glm::vec3> m_vertices;
-	std::vector<Edge> m_edges;
-	std::stack<Front*> m_fronts;
+	PolyContainer m_container;
 
+	MeshPtAdaptor m_mesh_adaptor;
+	FrontPtAdaptor m_front_adaptor;
+
+	Mesh_pt_kdtree m_mesh_tree;
+	Front_pt_kdtree m_front_tree;
 };
+
+
+
 
 
 #endif//POLYTRIS_HPP
