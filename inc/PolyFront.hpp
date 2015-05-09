945ecb1624e2c9f2f3b707253e8af94e9748d340
Evan Wilde <etcwilde@uvic.ca>
2015-05-08 20:52:23 -0700

    Added a pop vertex to the front
    I've implemented a popVertex to the front class. This removes a vertex from the
    front and updates the neighbor information for a given front vertex.
    
    I may add a new functionality that just kills the meaning of neighbors since
    the vertex will be removed when a new vertex builds on top of it and removes it
    from the front.
    
    We may need to adjust the implementation of the vertex structure though.

--- inc/PolyFront.hpp
+++ inc/PolyFront.hpp
@@ -21,24 +21,10 @@ public:
 		return m_verts[m_verts[i].neighbors[0]].v_index;
 	}
 
-	/*
-	inline unsigned int setNeighbor1Index(unsigned int i, unsigned int ni) const
-	{
-		m_verts[i].neighbors[0] = ni;
-	}
-	*/
-
 	inline unsigned int getRight(unsigned int i) const
 	{
 		return m_verts[m_verts[i].neighbors[1]].v_index;
 	}
-
-	/*
-	inline unsigned int setNeighbor2Index(unsigned int i unsigned int ni) const
-	{
-		m_verts[i].neighbors[1] = ni;
-	}
-	*/
 
 	inline unsigned int getVertex(unsigned int index) const
 	{ return m_verts[index].v_index; }
@@ -60,9 +46,7 @@ public:
 	// Index Vertex: Index in TrisPolyVerts
 	// neighbors: Index to verts in m_verts list
 	// Circularly-linked doubly-linked list implemented in an array-style
-	void pushVertex(unsigned int vertex_index,
-			float open_angle = 0,
-			float roc = 1)
+	void pushVertex(unsigned int vertex_index)
 	{
 		unsigned int my_index = m_verts.size();
 		unsigned int first_index = 0;
@@ -70,11 +54,8 @@ public:
 
 		FrontVertex f;
 		f.v_index = vertex_index;
-		f.open_angle = open_angle;
-		f.roc = roc;
-#ifdef DEBUG
-		std::cout << "Building Vertex" << vertex_index << ": " << '\n';
-#endif
+		f.open_angle = 0;
+		f.roc = 1;
 
 		if (m_verts.size() > 0)
 		{
@@ -87,31 +68,27 @@ public:
 
 			first->neighbors[0] = my_index;
 			last->neighbors[1] = my_index;
-			// HAHAHAHAHA 
-			// In case you are wondering, this is just pushing a
-			// vertex onto the back
 		}
-		else
-		{
-			// There are currently no vertices in the list
-			// Just insert ourselves and reference us
-			f.neighbors[0] = f.neighbors[1] = 0;
-
-		}
-
+		else f.neighbors[0] = f.neighbors[1] = 0;
 		m_verts.push_back(f);
-#ifdef DEBUG
-		for (unsigned int i = 0; i < m_verts.size(); ++i)
-		{
-			std::cout << i << '[' << m_verts[i].v_index << "] " << " : " << m_verts[i].neighbors[0]
-			<< ", " << m_verts[i].neighbors[1] << '\n';
-		}
-#endif
 	}
 
+	// Removes the last vertex
+	// Updates the neighbor links
+	// TODO: Check that this actually works
+	void popVertex()
+	{
+		if (m_verts.size() ==  0) return;
+		else if (m_verts.size() == 1) m_verts.pop_back();
+		unsigned int my_index = m_verts.size() - 1;
+		unsigned int first_index = 0;
+		unsigned int last_index = m_verts.size() - 2;
 
+		m_verts[last_index].neighbors[0] = first_index;
+		m_verts[first_index].neighbors[1] = last_index;
 
-
+		m_verts.pop_back();
+	}
 
 protected:
 
